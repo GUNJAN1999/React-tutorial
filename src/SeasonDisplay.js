5ff7f0cf8cea564d89e939098217467d10200690
GUNJAN1999 <gunjanbbps62@gmail.com>
2020-10-18 22:44:05 +0530

    Weather App

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const SeasonDisplay=()=>{
+	return (
+		<div>hi there how are you</div>
+	);
+};
+
+export default SeasonDisplay
--- src/index.js
+++ src/index.js
@@ -1,33 +1,13 @@
-//import the react and react dom library
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
 
-//create a react component
- const App=()=>{
-	 return (
-	 <div className="ui container comments">
-		 <ApprovalCard>
-			<CommentDetail 
-				author="sam"
-				timeAgo="TODAY 6:00pm"
-				content="NICE!!!"
-				avatar={faker.image.avatar()}
-			/>
-		 </ApprovalCard>
-		 <ApprovalCard>
-			<CommentDetail 
-				author="alex"
-				timeAgo="TODAY 6:00pm"
-				content="GOOD!!!"
-				avatar={faker.image.avatar()}
-			/>
-		</ApprovalCard>
-	 </div>
-	 );
- };
 
-//take the react component and show it on screen
+
+class App extends React.Component{
+
+	render(){
+		window.navigator.geolocation.getCurrentPosition((position)=> console.log(position),(err)=>console.log(err));
+		return <div>Latitude :</div>;
+	}
+}
 ReactDOM.render(<App/>,document.querySelector("#root"));
